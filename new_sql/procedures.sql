de74ff908e140cc02aae4e8daf2ef3898ab5315f
HDung1510 <167043485+HDung1510@users.noreply.github.com>
2024-12-17 15:55:08 +0700

    Update procedures.sql with count_dish, count_point, and some syntax

--- new_sql/procedures.sql
+++ new_sql/procedures.sql
@@ -1,199 +1,299 @@
-﻿DELIMITER $$
+DELIMITER $$
 
-CREATE PROCEDURE `add_customer` (
-    IN `cus_user` VARCHAR(16),
-    IN `cus_pass` VARCHAR(255),
-    IN `cus_name` VARCHAR(255),
-    IN `cus_add` VARCHAR(255),
-    IN `cus_phone` VARCHAR(10),
-    IN `cus_img` VARCHAR(255),
+CREATE PROCEDURE `add_customer` ( 
+    IN `cus_user` VARCHAR(16), 
+    IN `cus_pass` VARCHAR(255),    
+    IN `cus_name` VARCHAR(255), 
+    IN `cus_add` VARCHAR(255), 
+    IN `cus_phone` VARCHAR(10), 
+    IN `cus_img` VARCHAR(255), 
     IN `cus_point` INT
-)
-BEGIN
-    IF (EXISTS (SELECT * FROM `customer` WHERE `username` = `cus_user`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Tên đăng nhập đã tồn tại';
-    ELSEIF (LENGTH(`cus_pass`) < 8) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mật khẩu ít nhất phải có 8 ký tự';
-    ELSEIF (LENGTH(`cus_phone`) <> 10 || `cus_phone` NOT REGEXP "^0[0-9]{9}$") THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Số điện thoại phải bắt đầu với số 0 và bao gồm 10 chữ số';
-    ELSE
-        INSERT INTO `customer` (`username`, `password`, `name`, `address`, `phone`, `avatar`, `points`)
-        VALUES (`cus_user`, `cus_pass`, `cus_name`, `cus_add`, `cus_phone`, `cus_img`, `cus_point`);
-    END IF;
-END$$
-
-
-CREATE PROCEDURE `add_restaurant` (
-    IN `p_username` VARCHAR(16),
-    IN `p_password` VARCHAR(255),
-    IN `p_name` VARCHAR(255),
-    IN `p_address` VARCHAR(255),
-    IN `p_phone` VARCHAR(10),
-    IN `p_avatar` VARCHAR(255),
-    IN `p_description` TEXT
 )
 BEGIN
     IF EXISTS (
         SELECT 1 
-        FROM `customer` 
-        WHERE `username` = `p_username`
+        FROM customer 
+        WHERE username = cus_user
     ) OR EXISTS (
         SELECT 1 
-        FROM `restaurant` 
-        WHERE `username` = `p_username`
+        FROM restaurant 
+        WHERE username = cus_user
     ) THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Tên đăng nhập đã tồn tại.';
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Tên đăng nhập đã tồn tại';
+    ELSEIF (
+        LENGTH(cus_phone) <> 10 || cus_phone NOT REGEXP "^0[0-9]{9}$"
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Số điện thoại phải bắt đầu với số 0 và bao gồm 10 chữ số';
+    ELSE
+        INSERT INTO customer (username, password, name, address, phone, avatar, points)
+        VALUES (cus_user, cus_pass, cus_name, cus_add, cus_phone, cus_img, cus_point);
     END IF;
-    IF `p_phone` NOT REGEXP '^0[0-9]{9}$' THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Số điện thoại không hợp lệ. Số điện thoại phải bắt đầu bằng số 0 và có đúng 10 chữ số.';
-    END IF;
+END$$
 
-    INSERT INTO `restaurant` (`username`, `password`, `name`, `address`, `phone`, `avatar`, `description`)
-    VALUES (`p_username`, `p_password`, `p_name`, `p_address`, `p_phone`, `p_avatar`, `p_description`);
+
+CREATE PROCEDURE `add_restaurant` ( 
+    IN `res_user` VARCHAR(16), 
+    IN `res_pass` VARCHAR(255),
+    IN `res_name` VARCHAR(255), 
+    IN `res_add` VARCHAR(255), 
+    IN `res_phone` VARCHAR(10), 
+    IN `res_img` VARCHAR(255), 
+    IN `res_des` TEXT
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM customer 
+        WHERE username = res_user
+    ) OR EXISTS (
+        SELECT 1 
+        FROM restaurant 
+        WHERE username = res_user
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Tên đăng nhập đã tồn tại';
+    ELSEIF (
+        LENGTH(res_phone) <> 10 || res_phone NOT REGEXP "^0[0-9]{9}$"
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Số điện thoại phải bắt đầu với số 0 và bao gồm 10 chữ số';
+    ELSE
+        INSERT INTO restaurant (username, password, name, address, phone, avatar, description)
+        VALUES (res_user, res_pass, res_name, res_add, res_phone, res_img, res_des);
+    END IF;
 END$$
 
 
 CREATE PROCEDURE `add_dish` ( 
-    IN `d_name` VARCHAR(255), 
-    IN `d_code` VARCHAR(5), 
-    IN `d_des` TEXT, 
-    IN `d_price` DECIMAL(10, 2), 
-    IN `r_name` VARCHAR(16)
+    IN `dish_code` VARCHAR(5), 
+    IN `dish_name` VARCHAR(255), 
+    IN `dish_price` DECIMAL(10, 2), 
+    IN `restaurant_name` VARCHAR(16),
+    IN `dish_des` TEXT
 )
 BEGIN
-    IF (EXISTS (SELECT * FROM `dish` WHERE `id` = `d_code`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mã món ăn đã tồn tại';
-    ELSEIF (LENGTH(`d_code`) <> 5 OR `d_code` NOT REGEXP "^[a-zA-Z0-9]{5}") THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mã món ăn bao gồm 5 ký tự chữ hoặc số';
-    ELSEIF (`d_price` < 0) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Giá món ăn tối thiểu là 0';
-    ELSEIF (NOT EXISTS (SELECT * FROM `restaurant` WHERE `username` = `r_name`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nhà hàng không tồn tại';
+    IF EXISTS (
+        SELECT 1 
+        FROM dish 
+        WHERE id = dish_code
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Mã món ăn đã tồn tại';
+    ELSEIF (
+        LENGTH(dish_code) <> 5 || dish_code NOT REGEXP "^[a-zA-Z0-9]{5}"
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Mã món ăn bao gồm 5 ký tự chữ hoặc số';
+    ELSEIF (
+        dish_price < 0
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Giá món ăn tối thiểu là 0';
+    ELSEIF NOT EXISTS (
+        SELECT 1 
+        FROM restaurant 
+        WHERE name = restaurant_name
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Nhà hàng không tồn tại';
     ELSE
-        INSERT INTO `dish` (`id`, `name`, `price`, `restaurant`, `description`)
-        VALUES (`d_code`, `d_name`, `d_price`, `r_name`, `d_des`);
+        INSERT INTO dish (id, name, price, restaurant, description)
+        VALUES (dish_code, dish_name, dish_price, restaurant_name, dish_des);
     END IF;
 END$$
 
 
 CREATE PROCEDURE `delete_customer` ( 
-    IN `cus_user` VARCHAR(15)
+    IN `cus_user` VARCHAR(16)
 )
 BEGIN
-    IF (NOT EXISTS (SELECT * FROM `customer` WHERE `username` = `cus_user`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
+    IF NOT EXISTS (
+        SELECT 1
+        FROM customer 
+        WHERE username = cus_user
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
     ELSE
-        DELETE FROM `customer` WHERE `username` = `cus_user`;
+        DELETE FROM customer 
+        WHERE username = cus_user;
     END IF;
 END$$
 
-
 CREATE PROCEDURE `delete_restaurant` (
-    IN `p_username` VARCHAR(16)
+    IN `res_user` VARCHAR(16)
 )
 BEGIN
     IF NOT EXISTS (
         SELECT 1 
-        FROM `restaurant` 
-        WHERE `username` = `p_username`
+        FROM restaurant 
+        WHERE username = res_user
     ) THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại.';
+        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
+    ELSE
+        DELETE FROM customer 
+        WHERE username = res_user;
     END IF;
-
-    DELETE FROM `restaurant` WHERE `username` = `p_username`;
-END$$
+END $$
 
 
 CREATE PROCEDURE `delete_dish` ( 
-    IN `d_code` VARCHAR(15)
+    IN `dish_code` VARCHAR(5)
 )
 BEGIN
-    IF (NOT EXISTS (SELECT * FROM `dish` WHERE `id` = `d_code`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mã món ăn không tồn tại';
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM dish 
+        WHERE id = dish_code
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Mã món ăn không tồn tại';
     ELSE
-        DELETE FROM `dish` WHERE `id` = `d_code`;
+        DELETE FROM dish 
+        WHERE id = dish_code;
     END IF;
 END$$
 
 
 CREATE PROCEDURE `update_customer` ( 
-    IN `cus_user` VARCHAR(15), 
-    IN `cus_name` VARCHAR(256), 
-    IN `cus_add` VARCHAR(256), 
-    IN `cus_phone` VARCHAR(256), 
-    IN `cus_img` VARCHAR(256), 
+    IN `cus_user` VARCHAR(16), 
+    IN `cus_name` VARCHAR(255), 
+    IN `cus_add` VARCHAR(255), 
+    IN `cus_phone` VARCHAR(10), 
+    IN `cus_img` VARCHAR(255), 
     IN `cus_point` INT
 )
 BEGIN
-    IF (NOT EXISTS (SELECT * FROM `customer` WHERE `username` = `cus_user`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
-    ELSEIF (LENGTH(`cus_phone`) <> 10 OR `cus_phone` NOT REGEXP "^0[0-9]{9}$") THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Số điện thoại bắt đầu với số 0 và bao gồm 10 chữ số';
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM customer 
+        WHERE username = cus_user
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
+    ELSEIF (
+        LENGTH(cus_phone) <> 10 || cus_phone NOT REGEXP "^0[0-9]{9}$"
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Số điện thoại bắt đầu với số 0 và bao gồm 10 chữ số';
     ELSE
-        UPDATE `customer` 
-        SET name = `cus_name`, address = `cus_add`, `phone` = `cus_phone`, `avatar` = `cus_img`, `points` = `cus_point` 
-        WHERE `username` = `cus_user`;
+        UPDATE customer 
+        SET 
+            name = cus_name, 
+            address = cus_add, 
+            phone = cus_phone, 
+            avatar = cus_img, 
+            points = cus_point 
+        WHERE username = cus_user;
     END IF;
 END$$
 
 
 CREATE PROCEDURE `update_restaurant` (
-    IN `p_username` VARCHAR(16),
-    IN `p_password` VARCHAR(255),
-    IN `p_name` VARCHAR(255),
-    IN `p_address` VARCHAR(255),
-    IN `p_phone` VARCHAR(10),
-    IN `p_avatar` VARCHAR(255),
-    IN `p_description` TEXT
+    IN `res_user` VARCHAR(16), 
+    IN `res_name` VARCHAR(255), 
+    IN `res_add` VARCHAR(255), 
+    IN `res_phone` VARCHAR(10), 
+    IN `res_img` VARCHAR(255), 
+    IN `res_des` TEXT
 )
 BEGIN
     IF NOT EXISTS (
         SELECT 1 
-        FROM `restaurant` 
-        WHERE `username` = `p_username`
+        FROM restaurant 
+        WHERE username = res_user
     ) THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại.';
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Tên đăng nhập không tồn tại';
+    ELSEIF (
+        LENGTH(res_phone) <> 10 || res_phone NOT REGEXP "^0[0-9]{9}$"
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Số điện thoại bắt đầu với số 0 và bao gồm 10 chữ số';
+    ELSE
+        UPDATE restaurant 
+        SET 
+            name = res_name, 
+            address = res_add, 
+            phone = res_phone, 
+            avatar = res_img, 
+            description = res_des 
+        WHERE username = res_user;
     END IF;
-    IF `p_phone` NOT REGEXP '^0[0-9]{9}$' THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Số điện thoại không hợp lệ. Số điện thoại phải bắt đầu bằng số 0 và có đúng 10 chữ số.';
-    END IF;
-
-    UPDATE `restaurant`
-    SET
-        `password` = `p_password`,
-        `name` = `p_name`,
-        `address` = `p_address`,
-        `phone` = `p_phone`,
-        `avatar` = `p_avatar`,
-        `description` = `p_description`
-    WHERE `username` = `p_username`;
-END$$
+END $$
 
 
 CREATE PROCEDURE `update_dish` ( 
-    IN `d_name` VARCHAR(255), 
-    IN `d_code` VARCHAR(5), 
-    IN `d_des` TEXT, 
-    IN `d_price` DECIMAL(10, 2), 
-    IN `r_name` VARCHAR(16)
+    IN `dish_code` VARCHAR(5), 
+    IN `dish_name` VARCHAR(255), 
+    IN `dish_price` DECIMAL(10, 2), 
+    IN `restaurant_name` VARCHAR(16),
+    IN `dish_des` TEXT
 )
 BEGIN
-    IF (NOT EXISTS (SELECT * FROM `dish` WHERE `id` = `d_code`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mã món ăn không tồn tại';
-    ELSEIF (`d_price` < 0) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Giá món ăn tối thiểu là 0';
-    ELSEIF (NOT EXISTS (SELECT * FROM `restaurant` WHERE `username` = `r_name`)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nhà hàng không tồn tại';
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM dish 
+        WHERE id = dish_code
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Mã món ăn không tồn tại';
+    ELSEIF (
+        dish_price < 0
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Giá món ăn tối thiểu là 0';
+    ELSEIF NOT EXISTS (
+        SELECT 1 
+        FROM restaurant 
+        WHERE name = restaurant_name
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Nhà hàng không tồn tại';
     ELSE
-        UPDATE `dish` 
-        SET name = `d_name`, description = `d_des`, `price` = `d_price`, `restaurant` = `r_name` 
-        WHERE `id` = `d_code`;
+        UPDATE dish 
+        SET 
+            name = dish_name, 
+            price = dish_price, 
+            restaurant = restaurant_name,
+            description = dish_des
+        WHERE id = dish_code;
     END IF;
 END$$
+
+CREATE PROCEDURE `count_dish` (
+    IN `floor_price` DECIMAL(10, 2)
+)
+BEGIN
+	SELECT 
+        dish_group.name, 
+        floor_price, 
+        COUNT(*) AS quantity
+    FROM 
+        dish_group
+        JOIN dish_dish_group 
+            ON dish_dish_group.dish_group = dish_group.id
+        JOIN dish 
+            ON dish.id = dish_dish_group.dish     
+    WHERE dish.price >= floor_price
+    GROUP BY dish_group.name
+    HAVING quantity >= 1 
+    ORDER BY quantity, dish_group.name;
+END$$
+
+
+CREATE PROCEDURE `count_point` (
+    IN `point` INT
+)   
+BEGIN
+	SELECT username, name, address, phone, avatar, points 
+    FROM customer
+    WHERE points >= point
+    ORDER BY points;
+END$$
+
 
 DELIMITER ;
